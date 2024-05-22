3622def24140ee8ecaf1337602f791630561ae06
ryan <rhynogolfer42@gmail.com>
2024-05-22 11:32:30 -0700

    imported

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import IndexScreen from './src/screens/IndexScreen';
+import { BlogProvider } from './src/context/BlogContext';
 
 const Stack = createStackNavigator();
 
