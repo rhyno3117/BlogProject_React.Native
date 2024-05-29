114902fbf437f995070ca0fae2a4d237be5d383a
ryan <rhynogolfer42@gmail.com>
2024-05-29 15:56:42 -0700

    now showing data source of data

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,12 @@ import React, { createContext } from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-    return <BlogContext.Provider value={5}>
+const blogPosts = [
+    { title: 'Blog Post #1' },
+    { title: 'Blog Post #2' }
+];
+
+    return <BlogContext.Provider value={blogPosts}>
         {children}
     </BlogContext.Provider>
 };
