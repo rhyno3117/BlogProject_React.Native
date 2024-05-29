1c833137319e50bef78be5e8e007ca65e50b933b
ryan <rhynogolfer42@gmail.com>
2024-05-29 15:43:46 -0700

    error was happenign due to not importing createContext

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,8 +3,11 @@ import { View, Text, StyleSheet } from 'react-native'
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
+const value = useContext(BlogContext)
+
   return <View>
     <Text>Index Screen</Text>
+    <Text>{value}</Text>
   </View>
 }
 
