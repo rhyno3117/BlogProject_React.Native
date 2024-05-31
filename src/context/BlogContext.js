f49d2cc2e7553a0043dfa8139be9e146b3e5942e
ryan <rhynogolfer42@gmail.com>
2024-05-31 14:22:31 -0700

    added function that adds with updated state variable

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,14 +1,21 @@
-import React, { createContext } from 'react';
+import React, { useState, createContext } from 'react';
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-const blogPosts = [
+    const [blogPosts, setBlogPosts] = useState([]);
+
+    const addBlogPost = () => {
+        setBlogPosts([  ...blogPosts, {title: `Blog Post #${blogPosts.length + 1}`} ])
+    };
+
+
+const blogPostz = [
     { title: 'Blog Post #1' },
     { title: 'Blog Post #2' }
 ];
 
-    return <BlogContext.Provider value={blogPosts}>
+    return <BlogContext.Provider value={blogPostz}>
         {children}
     </BlogContext.Provider>
 };
