df4822b10e4ad9658c874e68ed9f675397152266
ryan <rhynogolfer42@gmail.com>
2024-06-10 19:54:10 -0700

    deleting works now with added deleteBlogPost added into IndexScreen useContext

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -4,7 +4,7 @@ import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
-  const { state, addBlogPost } = useContext(Context)
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context)
 
   return <View>
     <Button title="Add Post" onPress={addBlogPost} />
@@ -15,7 +15,7 @@ const IndexScreen = () => {
         return (
           <View style={styles.row}>
             <Text style={styles.title}>{item.title} - {item.id}</Text>
-            <TouchableOpacity onPress={() => console.log}>
+            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
               <Feather style={styles.icon} name="trash" />
             </TouchableOpacity>
           </View>
