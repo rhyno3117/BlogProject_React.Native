b284a33503f768daa16bd99ba4ac96791489c82e
ryan <rhynogolfer42@gmail.com>
2024-06-10 19:47:59 -0700

    added new case to reducer

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,6 +2,8 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
     switch (action.type) {
+        case 'delete_blogpost': 
+        return state.filter((blogPost) => blogPost.id !== action.payload)
         case 'add_blogpost':
             return [
                 ...state,
@@ -22,8 +24,8 @@ const addBlogPost = (dispatch) => {
 };
 
 const deleteBlogPost = (dispatch) => {
-    return () => {
-        dispatch({ type: 'delete_blogpost' })
+    return (id) => {
+        dispatch({ type: 'delete_blogpost', payload: id })
     }
 }
 
