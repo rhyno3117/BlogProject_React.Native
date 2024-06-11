ada291c871e0990c88f6223f936aa20020bb5095
ryan <rhynogolfer42@gmail.com>
2024-06-10 20:05:00 -0700

    added import of ShowScreen into app.js

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import IndexScreen from './src/screens/IndexScreen';
 import { Provider } from './src/context/BlogContext';
+import ShowScreen from './src/screens/ShowScreen';
 
 const Stack = createStackNavigator();
 
