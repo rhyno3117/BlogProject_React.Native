b38593415bfe49e705c6053006160f9d356dac0f
ryan <rhynogolfer42@gmail.com>
2024-06-11 21:11:14 -0700

    added route.params in ShowScreen to get id being passed from IndexScreen

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const ShowScreen = () => {
-    return <View>
-        <Text>Show Screen</Text>
-    </View>
+const ShowScreen = ({ route }) => {
+    const { id } = route.params;
+    console.log(id);
+
+    return (
+        <View>
+            <Text>Show Screen</Text>
+            <Text>Blog ID: {id}</Text>
+        </View>
+    );
 };
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
 
 export default ShowScreen;
