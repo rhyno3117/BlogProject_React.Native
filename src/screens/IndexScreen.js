efd520bb918358d77a32e642bfcef92cf8ec8982
ryan <rhynogolfer42@gmail.com>
2024-06-11 21:38:01 -0700

    used useLayoutEffect and setOptions to add the plus that will direct to create page

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,31 +1,43 @@
-import React, { useContext } from 'react'
-import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native'
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = ({ navigation }) => {
-  const { state, addBlogPost, deleteBlogPost } = useContext(Context)
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
-  return <View>
-    <Button title="Add Post" onPress={addBlogPost} />
-    <FlatList
-      data={state}
-      keyExtractor={(blogPost) => blogPost.title}
-      renderItem={({ item }) => {
-        return (
-          <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
-          <View style={styles.row}>
-            <Text style={styles.title}>{item.title} - {item.id}</Text>
-            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-              <Feather style={styles.icon} name="trash" />
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+          <Feather name="plus" size={30} style={{ marginRight: 10 }} />
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation]);
+
+  return (
+    <View>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={state}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
+              <View style={styles.row}>
+                <Text style={styles.title}>{item.title} - {item.id}</Text>
+                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                  <Feather style={styles.icon} name="trash" />
+                </TouchableOpacity>
+              </View>
             </TouchableOpacity>
-          </View>
-          </TouchableOpacity>
-        );
-      }}
-    />
-  </View>
-}
+          );
+        }}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   row: {
@@ -37,11 +49,11 @@ const styles = StyleSheet.create({
     borderColor: 'gray',
   },
   title: {
-    fontSize: 18
+    fontSize: 18,
   },
   icon: {
-    fontSize: 24
-  }
-})
+    fontSize: 24,
+  },
+});
 
 export default IndexScreen;
