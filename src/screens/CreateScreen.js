a9c0e765e6a102173b4cabb95823426cb139b187
ryan <rhynogolfer42@gmail.com>
2024-06-12 18:56:48 -0700

    used useContext and destructured addBlogPost and added it to onPress, now it is working and adding created blog post

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -5,6 +5,7 @@ import { Context } from '../context/BlogContext';
 const CreateScreen = ({ navigation }) => {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
+    const { addBlogPost } = useContext(Context);
 
     return (
         <View>
@@ -19,7 +20,10 @@ const CreateScreen = ({ navigation }) => {
                 value={content}
                 onChangeText={(text) => setContent(text)}
             />
-            <Button title="Add Blog Post"/>
+            <Button 
+            title="Add Blog Post"
+            onPress={() => addBlogPost(title, content)}
+            />
         </View>
     );
 };
