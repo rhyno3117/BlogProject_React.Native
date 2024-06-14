51d17b184de70a42d3161082fa247257d910d1ee
ryan <rhynogolfer42@gmail.com>
2024-06-13 18:09:44 -0700

    content loads when app loads/refreshes

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -34,6 +34,6 @@ const deleteBlogPost = (dispatch) => {
 export const { Context, Provider } = createDataContext(
     blogReducer,
     { addBlogPost, deleteBlogPost },
-    []
+    [{ title: 'TEST POST', content: 'TEST CONTENT', id: 1}]
 );
 
