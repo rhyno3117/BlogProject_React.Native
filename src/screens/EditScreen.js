3847ca74cd7dfe8f34a009a0293b2c1130be0810
ryan <rhynogolfer42@gmail.com>
2024-06-18 17:36:52 -0700

    added TextInput

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 import { Context } from '../context/BlogContext';
 
 const EditScreen = ({ route }) => {
@@ -10,6 +10,7 @@ const EditScreen = ({ route }) => {
     );
 
     const [title, setTitle] = useState(blogPost.title)
+    const [content, setContent] = useState(blogPost.content)
 
     return (
         <View>
