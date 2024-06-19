c07acc3a0a12d2d58f666eef24f676797cbc8999
ryan <rhynogolfer42@gmail.com>
2024-06-18 17:36:01 -0700

    added useContext also made state variable with default to title of blog

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Context } from '../context/BlogContext';
 
 const EditScreen = ({ route }) => {
     const { id } = route.params;
-    
+    const { state } = useContext(Context);
+
+    const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id')
+    );
+
+    const [title, setTitle] = useState(blogPost.title)
+
     return (
         <View>
             <Text>Edit Screen - {id}</Text>
