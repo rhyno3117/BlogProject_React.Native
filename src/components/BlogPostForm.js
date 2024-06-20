f29aea0db07bea3163d9b2ae928605497d0f2ded
ryan <rhynogolfer42@gmail.com>
2024-06-19 18:07:17 -0700

    added css

--- src/components/BlogPostForm.js
+++ src/components/BlogPostForm.js
@@ -27,6 +27,20 @@ const BlogPostForm = () => {
     );
 };
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    input: {
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: 'black',
+        marginBottom: 15,
+        padding: 5,
+        margin: 5
+    },
+    label: {
+        fontSize: 20,
+        marginBottom: 5,
+        marginLeft: 5,
+    }
+})
 
 export default BlogPostForm
--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -10,20 +10,6 @@ const CreateScreen = ({ navigation }) => {
     
 };
 
-const styles = StyleSheet.create({
-    input: {
-        fontSize: 18,
-        borderWidth: 1,
-        borderColor: 'black',
-        marginBottom: 15,
-        padding: 5,
-        margin: 5
-    },
-    label: {
-        fontSize: 20,
-        marginBottom: 5,
-        marginLeft: 5,
-    }
-});
+const styles = StyleSheet.create({});
 
 export default CreateScreen;
