5ca68e49a7642ce0fdf6807baa8897583ca8050c
ryan <rhynogolfer42@gmail.com>
2024-06-19 18:03:05 -0700

    cut the return js code from CreateScreen and inserted it in BlogPostForm

--- src/components/BlogPostForm.js
+++ src/components/BlogPostForm.js
@@ -2,7 +2,29 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const BlogPostForm = () => {
-
+    return (
+        <View>
+            <Text style={styles.label}>Enter Title:</Text>
+            <TextInput
+                style={styles.input}
+                value={title}
+                onChangeText={(text) => setTitle(text)} />
+            <Text style={styles.label}>Enter Content:</Text>
+            <TextInput
+                style={styles.input}
+                value={content}
+                onChangeText={(text) => setContent(text)}
+            />
+            <Button
+                title="Add Blog Post"
+                onPress={() => {
+                    addBlogPost(title, content, () => {
+                        navigation.navigate('Index');
+                    });
+                }}
+            />
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({})
