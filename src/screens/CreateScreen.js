7f53da8d18df0b0f4956f4577a27e4633289b89d
ryan <rhynogolfer42@gmail.com>
2024-06-19 18:10:45 -0700

    added import of BlogPostFOrm to createSCreen

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,9 +1,12 @@
 import React, { useContext, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Context } from '../context/BlogContext';
+import BlogPostForm from '../BlogPostForm';
 
 const CreateScreen = ({ navigation }) => {
     const { addBlogPost } = useContext(Context);
+
+    return <BlogPostForm />
 };
 
 const styles = StyleSheet.create({});
