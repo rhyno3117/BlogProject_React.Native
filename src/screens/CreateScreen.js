29842f56b915581e714df7c75757761d0ca36520
ryan <rhynogolfer42@gmail.com>
2024-06-19 18:28:09 -0700

    created onSubmit function in CreateScreen in the return of BlogPostForm

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -6,7 +6,9 @@ import BlogPostForm from '../components/BlogPostForm';
 const CreateScreen = ({ navigation }) => {
     const { addBlogPost } = useContext(Context);
 
-    return <BlogPostForm />
+    return <BlogPostForm onSubmit={() => {
+        
+    }} />
 };
 
 const styles = StyleSheet.create({});
